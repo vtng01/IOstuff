9e31d48854ff57b9ee6138120d0ced455b3a5ccc
vnguy4 <vanthoai.nguyen@aexp.com>
2021-10-29 09:51:43 +0100

    create zip file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        doTryCatchFinally();
+//        doTryCatchFinally();
+        try(FileSystem zipFs = openZip(Paths.get("myData.zip"))) {
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+
     }
 
     public static void doTryCatchFinally() {
@@ -20,5 +34,15 @@ public class Main {
         } catch (Exception e) {
             e.getStackTrace();
         }
+    }
+
+    public static FileSystem openZip(Path zipPath) throws IOException, URISyntaxException {
+        Map<String, String> providerProps = new HashMap<>();
+        providerProps.put("create", "true");
+
+        URI zipURI = new URI("jar:file", zipPath.toUri().getPath(), null);
+        FileSystem zipFs = FileSystems.newFileSystem(zipURI, providerProps);
+
+        return zipFs;
     }
 }
